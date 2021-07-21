1ac82c5ce8d0f7c9072bd1fc08b5cae80c81dc86
Arthur <osartie@gmail.com>
2021-07-22 02:13:01 +0300

    Сделан редизайн и обновлена верстка

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const addContact = document.querySelector('.add-contacts'),
+  const dialogBar = document.querySelector('.dialog-bar'),
+        dialogsBtn = document.querySelector('.menu-list__link_dialogs'),
+
+        addContact = document.querySelector('.menu-list__link_add'),
         overlay = document.querySelector('.overlay'),
         closeModal = document.querySelector('.close-modal'),
         modalPopup = document.querySelector('.modal-popup');
+
+  dialogsBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    dialogBar.classList.toggle('show');
+  });
+
+  // dialogsBtn.addEventListener('click', (event) => {
+  //   event.preventDefault();
+  //   if (dialogBar.classList.contains('show')) {
+  //     dialogBar.classList.remove('show')
+  //     dialogBar.classList.add('hide')
+  //   } else {
+  //     dialogBar.classList.remove('hide')
+  //     dialogBar.classList.add('show')
+  //   }
+  // })
 
   addContact.addEventListener('click', () => {
     overlay.classList.toggle('show');
@@ -21,3 +40,30 @@ document.addEventListener('DOMContentLoaded', function() {
   })
 
 });
+
+
+
+/*
+
+  // const addContact = document.querySelector('.add-contacts'),
+  //       overlay = document.querySelector('.overlay'),
+  //       closeModal = document.querySelector('.close-modal'),
+  //       modalPopup = document.querySelector('.modal-popup');
+
+  // addContact.addEventListener('click', () => {
+  //   overlay.classList.toggle('show');
+  //   modalPopup.classList.toggle('show');
+  // })
+
+  // overlay.addEventListener('click', () => {
+  //   overlay.classList.toggle('show');
+  //   modalPopup.classList.toggle('show');
+  // })
+
+  // closeModal.addEventListener('click', () => {
+  //   overlay.classList.toggle('show');
+  //   modalPopup.classList.toggle('show');
+  // })
+
+
+  */
